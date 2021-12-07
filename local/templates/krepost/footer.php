810180aaa9c44a1f2d8a3a7a3616227d1907fdd2
AleksinskyValery <AleksinskyValery@yandex.ru>
2021-12-07 20:16:53 +0400

    add old site stiles & scripts

--- local/templates/krepost/footer.php
+++ local/templates/krepost/footer.php
@@ -1,9 +1,210 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-</div>
+</main>
 
-<div class="footer">
-</div>
+<footer class="footer">
+    <div class="footer-info">
+        <div class="container container_p0">
+            <div class="footer-info__main">
+                <a href="/" class="footer-info__logo-link">
+                    <img class="footer-info__logo" src="<?= SITE_TEMPLATE_PATH ?>/img/main/logo.svg" alt="Группа компаний Крепость">
+                </a>
+                <ul class="footer-info__social-list">
+                    <li class="footer-info__social-item">
+                        <a href="https://instagram.com/krepost.73" class="footer-info__social-link" target="_blank">
+                            <img src="<?= SITE_TEMPLATE_PATH ?>/img/main/icons/footer-social-inst.svg" alt="Инстаграм" class="footer-info__social-img">
+                        </a>
+                    </li>
+                    <li class="footer-info__social-item">
+                        <a href="https://vk.com/krepost73" class="footer-info__social-link" target="_blank">
+                            <img src="<?= SITE_TEMPLATE_PATH ?>/img/main/icons/footer-social-vk.svg" alt="Вконтакте" class="footer-info__social-img">
+                        </a>
+                    </li>
+                    <li class="footer-info__social-item">
+                        <a href="https://ok.ru/group/54937615007827?st._aid=ExternalGroupWidget_OpenGroup" class="footer-info__social-link" target="_blank">
+                            <img src="<?= SITE_TEMPLATE_PATH ?>/img/main/icons/footer-social-ok.svg" alt="Одноклассники" class="footer-info__social-img">
+                        </a>
+                    </li>
+                </ul>
+                <ul class="footer-info__contacts-list">
+                    <li class="footer-info__contacts-item">
+                        <a href="tel:+78422265301" class="footer-info__contacts-link">+7 (8422) 26-53-01</a>
+                    </li>
+                    <li class="footer-info__contacts-item">
+                        <a href="mailto:krepost-gk@mail.ru" class="footer-info__contacts-link">krepost-gk@mail.ru</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-info__copyright-wrapper">
+                <p class="footer-info__copyright">© 2013 - 2021 Интернет магазин охранного оборудования "ГК "Крепость", Ульяновск</p>
+                <a href="https://agatech.ru/" class="footer-info__copyright-link" target="_blank">
+                    <img class="footer-info__copyright-img" src="<?= SITE_TEMPLATE_PATH ?>/img/main/footer-agatech.png" alt="AGATECH">
+                </a>
+            </div>
+        </div>
+    </div>
+    <nav class="footer-menu">
+        <div class="container container_p0">
+            <ul class="row footer-menu__list footer-menu__list_1920">
+                <li class="col-2 offset-1 offset-md-0 footer-menu__item">
+                    <ul class="footer-menu__sublist">
+                        <li class="footer-menu__subitem">
+                            <a href="/page/novelties.html" class="footer-menu__sublink">Новинки</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/actions.html#hit" class="footer-menu__sublink">Лидеры продаж</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/actions.html" class="footer-menu__sublink">Акции</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/sale.html" class="footer-menu__sublink">Распродажа</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/company.html" class="footer-menu__sublink">О компании</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/services.html" class="footer-menu__sublink">Услуги</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/documents.html" class="footer-menu__sublink">Документы</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/delivery.html" class="footer-menu__sublink">Доставка и оплата</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/contacts.html" class="footer-menu__sublink">Контакты</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="col-4 offset-1 footer-menu__item">
+                    <ul class="footer-menu__sublist">
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Domofonnye-sistemy" class="footer-menu__sublink">Домофонные системы</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Istochnik-elektropitaniya.-Akkumulyatory" class="footer-menu__sublink">Источники электропитания</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/svetilniki-avariinogo-osveshcheniya" class="footer-menu__sublink">Светильники аварийного освещения</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/sistema-videonabludeniya" class="footer-menu__sublink">Системы видеонаблюдения</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Sredstva-pojarotusheniya" class="footer-menu__sublink">Средства пожаротушения</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Rashodnye-i-montajnye-materialy.-Kabel-kanaly" class="footer-menu__sublink">Расходные и монтажные <br> материалы, кабель-каналы</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="col-4 offset-md-1 footer-menu__item">
+                    <ul class="footer-menu__sublist">
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Znaki-bezopasnosti" class="footer-menu__sublink">Знаки безопасности</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Kontrol-upravleniya-dostupom" class="footer-menu__sublink">Контроль управления доступом</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dispetcherskii-kompleks-Ob" class="footer-menu__sublink">Диспетчерский комплекс ОБЬ</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Kabelno-provodnikovaya-produkciya" class="footer-menu__sublink">Кабельно-проводниковая продукция</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Ohranno-pojarnaya-signalizaciya" class="footer-menu__sublink">Охранно-пожарная сигнализация</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dveri-protivopojarnye" class="footer-menu__sublink">Двери противожарные</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dopolnitelnye-ustroistva" class="footer-menu__sublink">Дополнительные устройства</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="row footer-menu__list footer-menu__list_576">
+                <li class="col-sm-5 col-6 footer-menu__item">
+                    <ul class="footer-menu__sublist">
+                        <li class="footer-menu__subitem">
+                            <a href="/page/novelties.html" class="footer-menu__sublink">Новинки</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/actions.html#hit" class="footer-menu__sublink">Лидеры продаж</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/actions.html" class="footer-menu__sublink">Акции</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/sale.html" class="footer-menu__sublink">Распродажа</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/company.html" class="footer-menu__sublink">О компании</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/services.html" class="footer-menu__sublink">Услуги</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/documents.html" class="footer-menu__sublink">Документы</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/delivery.html" class="footer-menu__sublink">Доставка и оплата</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/page/contacts.html" class="footer-menu__sublink">Контакты</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Domofonnye-sistemy" class="footer-menu__sublink">Домофонные системы</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Istochnik-elektropitaniya.-Akkumulyatory" class="footer-menu__sublink">Источники электропитания</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/sistema-videonabludeniya" class="footer-menu__sublink">Системы видеонаблюдения</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Sredstva-pojarotusheniya" class="footer-menu__sublink">Средства пожаротушения</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="col-6 offset--sm-1 footer-menu__item">
+                    <ul class="footer-menu__sublist">
+                        <li class="footer-menu__subitem">
+                            <a href="/category/svetilniki-avariinogo-osveshcheniya" class="footer-menu__sublink">Светильники аварийного освещения</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Rashodnye-i-montajnye-materialy.-Kabel-kanaly" class="footer-menu__sublink">Расходные и монтажные <br> материалы, кабель-каналы</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Znaki-bezopasnosti" class="footer-menu__sublink">Знаки безопасности</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Kontrol-upravleniya-dostupom" class="footer-menu__sublink">Контроль управления доступом</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dispetcherskii-kompleks-Ob" class="footer-menu__sublink">Диспетчерский комплекс ОБЬ</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Kabelno-provodnikovaya-produkciya" class="footer-menu__sublink">Кабельно-проводниковая продукция</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Ohranno-pojarnaya-signalizaciya" class="footer-menu__sublink">Охранно-пожарная сигнализация</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dveri-protivopojarnye" class="footer-menu__sublink">Двери противожарные</a>
+                        </li>
+                        <li class="footer-menu__subitem">
+                            <a href="/category/Dopolnitelnye-ustroistva" class="footer-menu__sublink">Дополнительные устройства</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</footer>
 
 </body>
 </html>
