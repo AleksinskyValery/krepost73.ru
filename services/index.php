a74b089b657e2b978b6260886839aecf0919855b
AleksinskyValery <AleksinskyValery@yandex.ru>
2021-12-12 23:12:10 +0400

    изменения на странице услуги

--- services/index.php
+++ services/index.php
@@ -1,4 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Услуги");
-?>Список услуг<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><div class="container">
+	 Список услуг
+</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
