842d4375f4e0e20d56f480be8d6884209d6be19e
AleksinskyValery1 <aleksinskyvalery@yandex.ru>
2022-02-10 06:55:38 +0000

    Переопределение фразы Пароль должен быть...

---
+++ local/php_interface/user_lang/ru/lang.php
@@ -0,0 +1,3 @@
+<?
+$MESS['/bitrix/modules/main/lang/ru/classes/general/user.php']['MAIN_GP_PASSWORD_LENGTH'] = 'Пароль должен быть не менее #LENGTH# символов';
+?>
--- local/templates/.default/components/bitrix/system.auth.authorize/.default/template.php
+++ local/templates/.default/components/bitrix/system.auth.authorize/.default/template.php
@@ -1,5 +1,7 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+LocalRedirect(SITE_DIR."auth/");
 ?><?
 ShowMessage($arParams["~AUTH_RESULT"]);
 ShowMessage($arResult['ERROR_MESSAGE']);
